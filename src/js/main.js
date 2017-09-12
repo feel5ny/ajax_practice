290d16f0acdc2352f14b696f6121c32f883a4cff
feel5ny <feel5.nayoung@gmail.com>
2017-09-12 11:23:11 +0900

    main.js수정 es6에 맞추어 수정

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,30 @@
 // import square from './lib';
 
 // input
-const userid = document.getElementById('userid')
-const password = document.getElementById('password')
-const firstname = document.getElementById('firstname')
-const lastname = document.getElementById('lastname')
+const userid = document.getElementById('userid');
+const password = document.getElementById('password');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
 
 // buttons
-const getBtn = document.getElementById('get')
-const postBtn = document.getElementById('post')
-const putBtn = document.getElementById('put')
-const deleteBtn = document.getElementById('del')
+const getBtn = document.getElementById('get');
+const postBtn = document.getElementById('post');
+const putBtn = document.getElementById('put');
+const deleteBtn = document.getElementById('del');
 
 // innerTEXT
-const codeView = document.getElementById('viewer')
-const message = document.getElementById('message')
+const codeView = document.getElementById('viewer');
+const message = document.getElementById('message');
 
 function getItem(e) {
   const xhr = new XMLHttpRequest();
   const email = /@/;
-  const useridVal = userid.value
+  const useridVal = userid.value;
   if (!useridVal.match(email)) {
     message.innerHTML = '다시 입력하세요';
+  } else {
+    message.innerHTML = '';
   }
-
   xhr.open('GET', `/users/${userid.value}`); // 비동기적인 GET요청을 연다.(open은 요청보낼 준비를 의미)
   xhr.send(); // 매개변수에는 요청에 쓸 데이터만 받는다.
   xhr.onreadystatechange = function () {
@@ -34,7 +35,7 @@ function getItem(e) {
         codeView.innerHTML = getItemList;
         // console.log(getItemList)
       } else {
-        console.log('status Error : ' + this.status);
+        console.log(`status Error : ${this.status}`);
       }
     }
   }
@@ -61,7 +62,7 @@ function postItem(e) {
         // console.log(postItemList)
         codeView.innerHTML = '포스트 완료 :) GET버튼을 눌러서 확인하세요'
       } else {
-        console.log("status Error : " + this.status);
+        console.log(`status Error : ${this.status}`);
       }
     }
   }
@@ -78,7 +79,7 @@ function deleteItem(e) {
         codeView.innerHTML = '삭제 완료 :) GET버튼을 눌러서 확인하세요'
         // console.log(deleteItemList)
       } else {
-        console.log("status Error : " + this.status);
+        console.log(`status Error : ${this.status}`);
       }
     }
   }
@@ -93,23 +94,21 @@ function putItem(e) {
     'password': password.value, 
     'firstname': firstname.value, 
     'lastname': lastname.value
-  };
+  }
   // console.log(data)
   xhr.send(JSON.stringify(data)); // 매개변수에는 요청에 쓸 데이터만 받는다.
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        console.log("put ok")
+        // console.log("put ok")
         const putItemList = JSON.stringify(JSON.parse(xhr.responseText), null, 2);
         // console.log(putItemList)
       } else {
-        console.log("status Error : " + this.status);
+        console.log(`status Error : ${this.status}`);
       }
     }
-  }
-} 
-  
-
+  };
+}
 
 // eventlistener
 getBtn.addEventListener('click', getItem)
