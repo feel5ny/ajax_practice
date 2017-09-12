e39f8314f7abb5cc527d2320e3c17db53d823c3d
feel5ny <feel5.nayoung@gmail.com>
2017-09-12 10:36:01 +0900

    upload files

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,116 @@
-import square from './lib';
+// import square from './lib';
 
-console.log(square(3)); // 9
+// input
+const userid = document.getElementById('userid')
+const password = document.getElementById('password')
+const firstname = document.getElementById('firstname')
+const lastname = document.getElementById('lastname')
 
-let html = '';
-square(3) === 9 ? html = 'Webpack is working!' : html = 'Somethig wrong!';
+// buttons
+const getBtn = document.getElementById('get')
+const postBtn = document.getElementById('post')
+const putBtn = document.getElementById('put')
+const deleteBtn = document.getElementById('del')
 
-document.getElementById('res').innerHTML = html;
+// codeview
+const codeView = document.getElementById('viewer')
+
+function getItem(e) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '/users/' + userid.value, true); // 비동기적인 GET요청을 연다.(open은 요청보낼 준비를 의미)
+  xhr.send(); // 매개변수에는 요청에 쓸 데이터만 받는다.
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // console.log("ok")
+        const getItemList = JSON.stringify(JSON.parse(xhr.responseText), null, 2); // xhr.responseText는 응답에 포함된 텍스트
+        codeView.innerHTML = getItemList
+        // console.log(getItemList)
+      } else {
+        console.log("status Error : " + this.status);
+      }
+    }
+  }
+}
+
+
+function postItem(e) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('post','/users',true); // 비동기적인 POST요청을 연다.(open은 요청보낼 준비를 의미)
+  xhr.setRequestHeader('Content-type', 'application/json');
+  const data = {
+    "userid" : userid.value, 
+    'password' : password.value, 
+    'firstname' : firstname.value, 
+    'lastname' : lastname.value
+  };
+  // console.log(data)
+  xhr.send(JSON.stringify(data)); // 매개변수에는 요청에 쓸 데이터만 받는다.
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        // console.log("ok")
+        const postItemList = JSON.stringify(JSON.parse(xhr.responseText), null, 2);
+        // console.log(postItemList)
+        codeView.innerHTML = "포스트 완료 :) GET버튼을 눌러서 확인하세요"
+      } else {
+        console.log("status Error : " + this.status);
+      }
+    }
+  }
+}
+  
+function deleteItem(e) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('delete','/users/'+ userid.value,true); // 비동기적인 DELETE요청을 연다.(open은 요청보낼 준비를 의미)
+  xhr.setRequestHeader('Content-type', 'application/json');
+  const data = {
+    "userid" : userid.value, 
+  };
+  xhr.send(JSON.stringify(data)); // 매개변수에는 요청에 쓸 데이터만 받는다.
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log("delete ok")
+        const deleteItemList = JSON.stringify(JSON.parse(xhr.responseText), null, 2); 
+        codeView.innerHTML = "삭제 완료 :) GET버튼을 눌러서 확인하세요"
+        // console.log(deleteItemList)
+      } else {
+        console.log("status Error : " + this.status);
+      }
+    }
+  }
+} 
+
+function putItem(e) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('put','/users/'+ userid.value,true); // 비동기적인 GET요청을 연다.(open은 요청보낼 준비를 의미)
+  xhr.setRequestHeader('Content-type', 'application/json');
+  const data = {
+    "userid" : userid.value, 
+    'password' : password.value, 
+    'firstname' : firstname.value, 
+    'lastname' : lastname.value
+  };
+  // console.log(data)
+  xhr.send(JSON.stringify(data)); // 매개변수에는 요청에 쓸 데이터만 받는다.
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log("put ok")
+        const putItemList = JSON.stringify(JSON.parse(xhr.responseText), null, 2);
+        // console.log(putItemList)
+      } else {
+        console.log("status Error : " + this.status);
+      }
+    }
+  }
+} 
+  
+
+
+// eventlistener
+getBtn.addEventListener('click', getItem)
+postBtn.addEventListener('click', postItem)
+putBtn.addEventListener('click', putItem)
+deleteBtn.addEventListener('click', deleteItem)
